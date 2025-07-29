e0f04655ee00614b88d57d6f5cd6c9b73aa83917
Mike C <mjcerchia@gmail.com>
2025-07-29 15:52:26 -0400

    Create 81-108-7-a-1.m

---
+++ Magma Code/81-108-7-a-1.m
@@ -0,0 +1,81 @@
+/****************************************************************************** 
+Here is a summary of the argument.
+
+Let C be the modular curve with lmfdb label 81.108.7.a.1. 
+We find there are three genus one quotients by an involution. 
+We are able to find a point on the second curve by intersecting with a hyperplane. From this, we are able to construct an elliptic curve, 
+which we find to have rank 1. 
+******************************************************************************/
+//The following model comes from code on David Zywina's Github
+
+P<[x]> := ProjectiveSpace(Rationals(),6);
+C := Curve(P,[
+    x[2]*x[3] - x[2]*x[4] + x[7]^2,
+    x[1]*x[2] + x[5]*x[7] - x[6]*x[7],
+    x[1]^2 - x[1]*x[2] + x[2]^2 - x[3]*x[7] + x[4]*x[7],
+    x[3]*x[4] + x[3]*x[5] + x[3]*x[6] + x[4]*x[6],
+    x[1]*x[7] - x[3]*x[5] + x[3]*x[6] + x[4]*x[5] - x[4]*x[6],
+    -x[1]^2 + x[3]*x[7] + x[4]*x[7] + x[5]*x[7] + 2*x[6]*x[7],
+    x[1]*x[5] - x[1]*x[6] + x[2]*x[3] + x[2]*x[4] + x[2]*x[5] + 2*x[2]*x[6],
+    2*x[1]*x[4] + 2*x[1]*x[5] + x[1]*x[6] - x[2]*x[5] + x[2]*x[6],
+    x[2]*x[7] - x[3]*x[4] - x[3]*x[5] + 2*x[4]^2 + 2*x[4]*x[5] + 2*x[4]*x[6],
+    -x[2]*x[7] - 2*x[3]*x[6] + 2*x[4]*x[5] + 3*x[5]^2 + 3*x[5]*x[6] + 3*x[6]^2
+]);
+
+S := AutomorphismGroup(C); 
+auts := [];
+Stemp := Automorphisms(C);
+for s in Stemp do
+auts := Append(auts, S!s);
+end for;
+#auts eq #S;
+
+//There are three genus one quotients by an involution
+l := []; //list of genus 1 quotients by involutions
+m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't be in the same list.
+for g in auts do
+if Order(g) eq 2 then
+AG := AutomorphismGroup(C,[g]);
+CG,prj := CurveQuotient(AG);prj;
+if Genus(CG) eq 1 then
+try
+l := Append(l,CG);
+catch e
+m := Append(m,CG);
+end try;
+end if;
+CG; Genus(CG);
+end if;
+print ".........";
+end for;
+
+//One of these quotients has the following model:
+P<[x]> := ProjectiveSpace(Rationals(),7);
+C1 := Curve(P,[x[1]^2 - 2*x[3]^2 + 8*x[6]^2 + x[7]^2 + 8*x[5]*x[8],
+x[1]*x[2] - x[6]^2 + x[8]^2,
+8*x[1]*x[4] + x[7]^2 + 8*x[5]*x[8],
+x[1]*x[5] - 8*x[4]*x[5] - x[3]*x[7],
+4*x[1]*x[6] + 32*x[2]*x[6] + x[5]*x[7],
+-2*x[3]*x[5] + x[1]*x[7] + 8*x[6]*x[8],
+-x[6]*x[7] + x[1]*x[8] - 8*x[4]*x[8],
+32*x[2]^2 + 4*x[6]^2 + x[5]*x[8] - 4*x[8]^2,
+8*x[2]*x[3] + x[7]*x[8],
+8*x[2]*x[4] + x[8]^2,
+x[2]*x[5] - x[4]*x[8],
+x[2]*x[7] - x[6]*x[8],
+-2*x[4]*x[5] + x[6]*x[7] + 8*x[2]*x[8] + 8*x[4]*x[8],
+8*x[3]*x[4] + x[5]*x[7],
+8*x[3]*x[6] + x[7]^2,
+-x[5]*x[6] + x[3]*x[8],
+8*x[4]^2 + x[5]*x[8],
+8*x[4]*x[6] + x[7]*x[8],
+-x[5]*x[6] + x[4]*x[7],
+2*x[5]^2 - x[7]^2 - 8*x[5]*x[8] - 8*x[8]^2]);
+
+//We can't immediately find a point, so we intersect with a hyperplane
+Points(C1 meet Scheme(AmbientSpace(C1),x[1]));
+
+//This produces a point
+pt := C1![0,-1/4,-2,1/2,-2,1,-4,1];
+E := EllipticCurve(C1,pt);
+Rank(E); //1
