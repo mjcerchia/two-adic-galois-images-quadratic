c48187ef99fd9c3b44746e4a3fb0087153a3ce1c
Mike C <mjcerchia@gmail.com>
2025-07-30 16:17:13 -0400

    Update 37-114-4-a-1NOTDONE.m

--- Magma Code/37-114-4-a-1NOTDONE.m
+++ Magma Code/37-114-4-a-1NOTDONE.m
@@ -39,3 +39,54 @@ end for;
 
 //One of these quotients has the following model:
 C1:=l[3];
+
+//This is quite complicated, so we search for divisors to construct a simpler model.
+P<[X]> := AmbientSpace(C1);
+deg := 3;
+tups := {};
+d := Dimension(P);
+ideals := {@@};
+tupsOld := tups;
+coeffs := [ 0, 1 ];
+    tups   :=
+        [ P![ tup[i] : i in [1..#tup] ] :
+          tup in CartesianPower(coeffs, d+1) |
+          not tup eq < 0 : i in [1..d+1] >  
+          and not P![tup[i] : i in [1..#tup]] in tupsOld
+        ];
+    for c in tups  do  
+            L := Scheme(P,&+[c[i]*X[i] : i in [1..Dimension(AmbientSpace(C1)) + 1]]);
+              irr := IrreducibleComponents(L meet C1);
+              degs := [Degree(ReducedSubscheme(i)) : i in irr];
+              if  deg in degs then
+        for cpt in irr do  
+         if  Degree(ReducedSubscheme(cpt)) eq deg
+             and not Basis(Ideal(ReducedSubscheme(cpt))) in ideals then      
+               ideals := ideals join {@Basis(Ideal(ReducedSubscheme(cpt)))@}; 
+        end if;
+    end for;
+    end if;
+    end for;
+    ideals;
+
+//Using a degree three divisor found above, we build a new model using Riemann-Roch.
+gens := ideals[1];
+P7 := AmbientSpace(C1);
+R  := CoordinateRing(P7);
+I := ideal< R | gens >;
+Pl := Place(C1, I);
+D  := Divisor(Pl);
+a,b := RiemannRochSpace(D);
+x1 := b(a.1); 
+x2 := b(a.2);
+x3:=b(a.3); 
+F := BaseField(C1);
+P := PolynomialRing( F, [1,1,1], "grevlexw", [1,1,1] );
+vars := ["x", "y","z"]; 
+P := ProjectiveSpace(P);
+AssignNames(~P, vars);
+phi := map<C1 -> P | [x1,x2,x3]>;
+C2:=Image(phi); 
+
+E := EllipticCurve(C2);
+    
