7576661e6ed3c4d9b6070358a5a7897cee007835
Mike C <mjcerchia@gmail.com>
2025-07-30 15:13:43 -0400

    Update 37-114-4-a-1NOTDONE.m

--- Magma Code/37-114-4-a-1NOTDONE.m
+++ Magma Code/37-114-4-a-1NOTDONE.m
@@ -1,2 +1,39 @@
-37.114.4.a.1
-//Check that this is in fact bielliptic. 
+
+/****************************************************************************** 
+Here is a summary of the argument.
+
+Let C be the modular curve with lmfdb label 37.114.4.a.1. 
+We find there are four genus one quotients by an involution. 
+
+******************************************************************************/
+P<x,y,z> := ProjectiveSpace(Rationals(),2);
+C := Curve(P,[2*x^3*y + x^3*z + 3*x^2*y*z + 2*x*y^3 + 3*x*y^2*z - 2*x*z^3 + y^3*z - 2*y*z^3 - z^4]);
+
+S := AutomorphismGroup(C); 
+auts := [];
+Stemp := Automorphisms(C);
+for s in Stemp do
+auts := Append(auts, S!s);
+end for;
+#auts eq #S;
+
+//There are three genus one quotients by an involution
+l := []; //list of genus 1 quotients by involutions
+m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't be in the same list.
+for g in auts do
+if Order(g) eq 2 then
+AG := AutomorphismGroup(C,[g]);
+CG,prj := CurveQuotient(AG);prj;
+if Genus(CG) eq 1 then
+try
+l := Append(l,CG);
+catch e
+m := Append(m,CG);
+end try;
+end if;
+CG; Genus(CG);
+end if;
+print ".........";
+end for;
+
+//One of these quotients has the following model:
