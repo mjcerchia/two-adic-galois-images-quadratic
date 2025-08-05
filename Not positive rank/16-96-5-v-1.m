bf2001a7fa48fd782e48eeca7d9f1a04c0b2459e
Mike C <mjcerchia@gmail.com>
2025-08-05 18:17:13 -0400

    Update and rename 16-96-5-v-1ND.m to 16-96-5-v-1.m

---
+++ Not positive rank/16-96-5-v-1.m
@@ -0,0 +1,47 @@
+16.96.5.v.1
+/****************************************************************************** 
+Here is a summary of the argument.
+
+Let C be the modular curve with lmfdb label 16.96.5.v.1.
+We compute the automorphism group over F3 and find there is one genus one quotients by an involution. It has 4 points, while the
+single rank one factor of the Jacobian has 6 points.
+NOT bielliptic. 
+******************************************************************************/
+P<x,y,z,w,t> := ProjectiveSpace(Rationals(),4);
+C := Curve(P,[2*x^2 + z*t, y^2 + y*z - y*t + z*w - z*t + w^2 - w*t, 2*y^2 + y*z - 2*y*w - y*t - z^2 - z*w + w*t]);
+C3 := Curve(Reduction(C,3));
+
+S := AutomorphismGroup(C3); 
+auts := [];
+Stemp := Automorphisms(C3);
+for s in Stemp do
+auts := Append(auts, S!s);
+end for;
+#auts eq #S;
+
+
+//There is one genus one quotients by an involution
+l := []; //list of genus 1 quotients by involutions
+m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't be in the same list.
+for g in auts do
+if Order(g) eq 2 then
+AG := AutomorphismGroup(C3,[g]);
+CG,prj := CurveQuotient(AG);prj;
+if Genus(CG) eq 1 then
+try
+l := Append(l,CG);
+catch e
+m := Append(m,CG);
+end try;
+end if;
+CG; Genus(CG);
+end if;
+print ".........";
+end for;
+
+l;
+
+#EllipticCurve(Curve(l[1])); //4
+Qx<x> := PolynomialRing(Rationals());
+E := EllipticCurve(x^3+x^2+x+1);
+#EllipticCurve(Curve(Reduction(E,3))); //6
