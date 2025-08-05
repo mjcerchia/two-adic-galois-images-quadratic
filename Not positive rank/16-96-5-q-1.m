c3794b8ca224007f7fda9290d05646cdaa33418c
Mike C <mjcerchia@gmail.com>
2025-08-05 17:47:16 -0400

    Update and rename 16-96-5-q-1ND.m to 16-96-5-q-1.m

--- Not positive rank/16-96-5-q-1.m
+++ Not positive rank/16-96-5-q-1.m
@@ -4,7 +4,7 @@ Here is a summary of the argument.
 
 Let C be the modular curve with lmfdb label 16.96.5.q.1
 We compute the automorphism group over F3 and find there is one genus one quotient by an involution. It has 4 points.
-The single rank one elliptic curve factor of the Jacobian has 8 points over F3.
+The single rank one elliptic curve factor of the Jacobian has 6 points over F3.
 NOT bielliptic. 
 ******************************************************************************/
 P<x,y,z,w,t>:=ProjectiveSpace(Rationals(),4);
@@ -41,10 +41,33 @@ end for;
 
 l;
 \*
+[
+    Curve over Rational Field defined by
+    x[1]^2 + x[4]^2 - 2*x[6]^2 - x[7]^2,
+    x[1]*x[2] - x[6]^2,
+    x[1]*x[3] - x[7]^2,
+    x[1]*x[5] - x[4]*x[6],
+    x[1]*x[6] - x[4]*x[7],
+    x[4]*x[6] + x[1]*x[7] - x[3]*x[7] - 2*x[6]*x[8],
+    -x[6]*x[7] + x[1]*x[8],
+    2*x[2]^2 - x[5]^2 - x[6]^2 + x[8]^2,
+    x[2]*x[3] - x[8]^2,
+    x[2]*x[4] - x[5]*x[6],
+    x[2]*x[6] - x[5]*x[8],
+    x[2]*x[7] - x[6]*x[8],
+    -x[5]*x[6] - x[6]*x[7] + 2*x[2]*x[8] + x[3]*x[8],
+    x[3]^2 - x[6]^2 - x[7]^2 + 2*x[8]^2,
+    x[3]*x[4] - x[6]*x[7],
+    x[3]*x[5] - x[6]*x[8],
+    x[3]*x[6] - x[7]*x[8],
+    x[4]*x[5] + x[4]*x[7] - 2*x[5]*x[8] - x[7]*x[8],
+    -x[6]^2 + x[4]*x[8],
+    -x[6]^2 + x[5]*x[7]
+]
 
 */
 #EllipticCurve(Curve(Reduction(l[1],3))); 
 
 Qx<x> := PolynomialRing(Rationals());
 E := EllipticCurve(x^3+x^2+x+1);
-#EllipticCurve(Curve(Reduction(E,3))); //8
+#EllipticCurve(Curve(Reduction(E,3))); //6
