ad859932361ed12e3100baa4508149be57aa947a
Mike C <mjcerchia@gmail.com>
2025-08-05 14:37:53 -0400

    Update and rename 16-96-5-ea-1ND.m to 16-96-5-ea-1.m

---
+++ Not positive rank/16-96-5-ea-1.m
@@ -0,0 +1,42 @@
+16.96.5.ea.1
+/****************************************************************************** 
+Here is a summary of the argument.
+
+Let C be the modular curve with lmfdb label 16.96.5.ea.1.
+We compute the automorphism group over Q and find there are no genus one quotients by an involution.
+NOT bielliptic. 
+******************************************************************************/
+P<x,y,z,w,t> := ProjectiveSpace(Rationals(),4);
+C := Curve(P,[-x*w + x*t + y*z, 4*x^2 + y^2 + 2*z^2 + w^2 - w*t, 4*x^2 - y^2 - 2*z^2 - 2*w^2 + 3*w*t + t^2]);
+
+S := AutomorphismGroup(C); 
+
+S := AutomorphismGroup(C); 
+auts := [];
+Stemp := Automorphisms(C);
+for s in Stemp do
+auts := Append(auts, S!s);
+end for;
+#auts eq #S;
+
+
+//There are no genus one quotients by an involution
+l := []; //list of genus 1 quotients by involutions
+m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't be in the same list.
+for g in auts do
+if Order(g) eq 2 then
+AG := AutomorphismGroup(C,[g]);
+CG,prj := CurveQuotient(AG);prj;
+if Genus(CG) eq 1 then
+try
+l := Append(l,CG);
+catch e
+m := Append(m,CG);
+end try;
+end if;
+CG; Genus(CG);
+end if;
+print ".........";
+end for;
+
+l;
