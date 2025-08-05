00e7b4b3519370da09f54b37b006d069e0d8b373
Mike C <mjcerchia@gmail.com>
2025-08-05 14:27:45 -0400

    Update 16-96-5-cy-1.m

--- Not positive rank/16-96-5-cy-1.m
+++ Not positive rank/16-96-5-cy-1.m
@@ -21,7 +21,7 @@ end for;
 #auts eq #S;
 
 
-//There is one genus one quotients by an involution
+//There are two genus one quotients by an involution
 l := []; //list of genus 1 quotients by involutions
 m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't be in the same list.
 for g in auts do
