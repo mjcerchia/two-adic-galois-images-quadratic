9dc21f71f42a9cd6ee0216d8090c5796a519624c
Mike C <mjcerchia@gmail.com>
2025-08-06 11:36:21 -0400

    Update and rename 32-96-5-v-1ND.m to 32-96-5-v-1.m

---
+++ Not positive rank/32-96-5-v-1.m
@@ -0,0 +1,74 @@
+32.96.5.v.1
+/****************************************************************************** 
+Here is a summary of the argument.
+
+Let C be the modular curve with lmfdb label 32.96.5.q.1.
+We compute the automorphism group over Q and find there is one genus one quotient by an involution. Over F7, it has 8 points, 
+while the single rank one elliptic curve factor of the Jacobian has 12 points over F7.
+NOT bielliptic. 
+******************************************************************************/
+P<x,y,z,w,t>:=ProjectiveSpace(Rationals(),4);
+C := Curve(P,[2*x^2 - y*t - 2*z*t, y^2 + 2*y*z - 2*y*w + 2*z^2, y*w + 4*w^2 - t^2]);
+S := AutomorphismGroup(C); 
+
+auts := [];
+Stemp := Automorphisms(C);
+for s in Stemp do
+auts := Append(auts, S!s);
+end for;
+#auts eq #S;
+
+
+//There is one genus one quotient by an involution
+l := []; //list of genus 1 quotients by involutions
+m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't be in the same list.
+for g in auts do
+if Order(g) eq 2 then
+AG := AutomorphismGroup(C,[g]);
+CG,prj := CurveQuotient(AG);prj;
+if Genus(CG) eq 1 then
+try
+l := Append(l,CG);
+catch e
+m := Append(m,CG);
+end try;
+end if;
+CG; Genus(CG);
+end if;
+print ".........";
+end for;
+
+l;
+\*
+[
+    Curve over Rational Field defined by
+    48*x[1]^2 - 8*x[1]*x[3] + x[3]^2 + 2*x[4]*x[6] - 20*x[6]^2 + 2*x[7]^2,
+    x[1]*x[2] - x[7]^2,
+    x[1]*x[4] - x[3]*x[6],
+    x[1]*x[5] - x[4]*x[6],
+    x[3]*x[4] + 48*x[1]*x[6] - 8*x[3]*x[6] + 2*x[5]*x[6] - 20*x[6]*x[7] + 
+        2*x[7]*x[8],
+    -x[6]^2 + x[1]*x[7],
+    -x[6]*x[7] + x[1]*x[8],
+    2*x[2]^2 + 2*x[2]*x[5] + x[5]^2 - 8*x[5]*x[7] + 48*x[7]^2 - 20*x[8]^2,
+    x[2]*x[3] - x[5]*x[7],
+    x[2]*x[4] - x[5]*x[8],
+    x[2]*x[6] - x[7]*x[8],
+    x[2]*x[7] - x[8]^2,
+    x[4]*x[5] - 8*x[5]*x[6] + 48*x[6]*x[7] + 2*x[2]*x[8] + 2*x[5]*x[8] - 
+        20*x[7]*x[8],
+    x[3]*x[5] - 8*x[4]*x[6] + 48*x[6]^2 + 2*x[5]*x[7] - 20*x[7]^2 + 2*x[8]^2,
+    -x[4]*x[6] + x[3]*x[7],
+    -x[5]*x[6] + x[3]*x[8],
+    x[4]^2 - 8*x[4]*x[6] + 48*x[6]^2 + 2*x[5]*x[7] - 20*x[7]^2 + 2*x[8]^2,
+    -x[5]*x[6] + x[4]*x[7],
+    -x[5]*x[7] + x[4]*x[8],
+    -x[7]^2 + x[6]*x[8]
+]
+*/
+
+#EllipticCurve(Curve(Reduction((l[1]),7))); //8
+
+Qx<x> := PolynomialRing(Rationals());
+E := EllipticCurve(x^3+x^2+x+1);
+#EllipticCurve(Curve(Reduction(E,7))); //12
