2270825890e0c6c70ecde57fec6bb4e820626663
Mike C <mjcerchia@gmail.com>
2025-08-06 11:01:47 -0400

    Update 32-96-5-f-1ND.m

--- Not positive rank/32-96-5-f-1ND.m
+++ Not positive rank/32-96-5-f-1ND.m
@@ -3,9 +3,9 @@
 Here is a summary of the argument.
 
 Let C be the modular curve with lmfdb label 32.96.5.f.1.
-We compute the automorphism group over Q and find there are three genus one quotient by an involution. Over F5, the second and third each
-have 4 points, while the single rank one elliptic curve factor of the Jacobian has 8 points over F5. Over F7, the first has 8 points, while
-the single rank one elliptic curve factor of the Jacobian has 12 points.
+We compute the automorphism group over Q and find there are three genus one quotient by an involution. Over F5, the first and second
+have 4 and 2 points, respectively, while the single rank one elliptic curve factor of the Jacobian has 10 points over F5. This leaves us 
+with the second curve. We construct a new model of this quotient curve using Riemann-Roch and find that it fails local solubility. 
 NOT bielliptic. 
 ******************************************************************************/
 P<x,y,z,w,t>:=ProjectiveSpace(Rationals(),4);
@@ -43,91 +43,103 @@ l;
 \*
 [
     Curve over Rational Field defined by
-    x[1]^2 + x[4]^2 + 2*x[6]*x[7],
-    x[1]*x[2] - x[6]^2,
-    x[1]*x[3] - x[7]^2,
+    128*x[1]^2 + x[3]^2 + 2*x[7]^2,
+    x[1]*x[2] - x[7]^2,
+    x[1]*x[4] - x[3]*x[6],
     x[1]*x[5] - x[4]*x[6],
-    x[1]*x[6] - x[4]*x[7],
-    x[4]*x[6] + x[1]*x[7] + 2*x[7]*x[8],
+    x[3]*x[4] + 128*x[1]*x[6] + 2*x[7]*x[8],
+    -x[6]^2 + x[1]*x[7],
     -x[6]*x[7] + x[1]*x[8],
-    x[2]*x[3] - x[8]^2,
-    x[2]*x[4] + x[6]*x[7] + 2*x[8]^2,
-    x[2]*x[6] - x[5]*x[8],
+    2*x[2]^2 + x[5]^2 + 128*x[7]^2,
+    x[2]*x[3] - x[5]*x[7],
+    x[2]*x[4] - x[5]*x[8],
+    x[2]*x[6] - x[7]*x[8],
+    x[2]*x[7] - x[8]^2,
+    x[4]*x[5] + 128*x[6]*x[7] + 2*x[2]*x[8],
+    x[3]*x[5] + 128*x[6]^2 + 2*x[8]^2,
+    -x[4]*x[6] + x[3]*x[7],
+    -x[5]*x[6] + x[3]*x[8],
+    x[4]^2 + 128*x[6]^2 + 2*x[8]^2,
+    -x[5]*x[6] + x[4]*x[7],
+    -x[5]*x[7] + x[4]*x[8],
+    -x[7]^2 + x[6]*x[8],
+    Curve over Rational Field defined by
+    2048*x[1]^2 + 16*x[3]^2 + x[5]*x[8],
+    512*x[1]*x[2] - x[5]*x[8],
+    16*x[1]*x[4] + 4*x[7]^2 - x[8]^2,
+    4*x[1]*x[5] - 4*x[3]*x[7] + x[4]*x[8],
+    512*x[1]*x[6] - x[5]*x[7],
+    x[3]*x[5] + 128*x[1]*x[7],
+    x[4]*x[5] + 128*x[1]*x[8],
+    128*x[2]^2 - 512*x[6]^2 + x[5]*x[8],
+    4*x[2]*x[3] + x[7]*x[8],
+    4*x[2]*x[4] + x[8]^2,
+    x[2]*x[5] - x[4]*x[8],
     x[2]*x[7] - x[6]*x[8],
-    x[5]^2 + x[6]^2 + 2*x[2]*x[8],
-    x[3]*x[4] - x[6]*x[7],
-    x[3]*x[5] - x[6]*x[8],
-    x[3]*x[6] - x[7]*x[8],
-    x[6]^2 + x[7]^2 + 2*x[3]*x[8],
-    x[4]*x[5] + x[4]*x[7] + 2*x[6]*x[8],
-    -x[6]^2 + x[4]*x[8],
-    x[5]*x[6] + x[6]*x[7] + 2*x[8]^2,
-    -x[6]^2 + x[5]*x[7],
+    -x[4]*x[5] - 128*x[6]*x[7] + 32*x[2]*x[8],
+    4*x[3]*x[4] + x[5]*x[7],
+    4*x[3]*x[6] + x[7]^2,
+    -x[5]*x[6] + x[3]*x[8],
+    4*x[4]^2 + x[5]*x[8],
+    4*x[4]*x[6] + x[7]*x[8],
+    -x[5]*x[6] + x[4]*x[7],
+    x[5]^2 + 128*x[7]^2 - 32*x[8]^2,
     Curve over Rational Field defined by
-    48*x[1]^2 - 288*x[1]*x[4] + 480*x[4]^2 - 672*x[4]*x[5] - 48*x[5]^2 - 
-        12*x[6]*x[7] + 100*x[7]^2 - 27*x[6]*x[8] - 6*x[7]*x[8] - 3*x[8]^2,
-    3*x[1]*x[2] - 3*x[6]*x[7] - 8*x[7]^2 + 3*x[6]*x[8] + 2*x[7]*x[8],
-    4*x[1]*x[3] - 8*x[5]^2 - x[7]*x[8] + x[8]^2,
-    24*x[1]*x[5] - 24*x[4]*x[5] + 24*x[5]^2 + 3*x[6]*x[7] - 10*x[7]^2 + 
-        3*x[7]*x[8],
-    18*x[1]*x[6] + 33*x[3]*x[6] - 60*x[4]*x[6] + 66*x[5]*x[6] + 20*x[4]*x[7] - 
-        88*x[5]*x[7] + 36*x[4]*x[8] - 38*x[5]*x[8],
-    3*x[3]*x[6] - 12*x[4]*x[6] + 24*x[5]*x[6] + 12*x[1]*x[7] - 32*x[4]*x[7] + 
-        40*x[5]*x[7] + 8*x[5]*x[8],
-    3*x[3]*x[6] - 3*x[5]*x[6] - 8*x[5]*x[7] + 3*x[1]*x[8] + 2*x[5]*x[8],
-    3*x[2]^2 - 12*x[6]*x[8] - 8*x[7]*x[8],
-    x[2]*x[3] - x[8]^2,
-    4*x[2]*x[4] - 4*x[7]^2 - 4*x[7]*x[8] - x[8]^2,
-    x[2]*x[5] - x[7]*x[8],
-    9*x[2]*x[6] - 36*x[3]*x[6] - 48*x[5]*x[6] + 112*x[5]*x[7] - 72*x[4]*x[8] + 
-        48*x[5]*x[8],
-    -12*x[5]*x[6] + 3*x[2]*x[7] - 8*x[5]*x[7],
-    -12*x[3]*x[6] + 3*x[2]*x[8] - 8*x[5]*x[8],
-    6*x[3]^2 - 24*x[5]^2 - 3*x[6]*x[8] + 4*x[7]*x[8] + 3*x[8]^2,
-    24*x[3]*x[4] - 48*x[4]*x[5] - 6*x[6]*x[7] + 8*x[7]^2 - 3*x[6]*x[8] + 
-        10*x[7]*x[8] + 3*x[8]^2,
-    12*x[3]*x[5] - 24*x[4]*x[5] + 24*x[5]^2 - 3*x[6]*x[7] + 4*x[7]^2 + 
-        3*x[7]*x[8],
-    x[3]*x[7] - x[5]*x[8],
-    4*x[5]*x[7] + x[3]*x[8] - 4*x[4]*x[8] + 4*x[5]*x[8],
-    18*x[6]^2 - 12*x[6]*x[7] + 20*x[7]^2 - 18*x[6]*x[8] - 12*x[7]*x[8] - 
-        9*x[8]^2,
-    Curve over Rational Field defined by
-    48*x[1]^2 - 288*x[1]*x[4] + 480*x[4]^2 + 672*x[4]*x[5] - 48*x[5]^2 + 
-        12*x[6]*x[7] - 100*x[7]^2 - 27*x[6]*x[8] - 6*x[7]*x[8] + 3*x[8]^2,
-    3*x[1]*x[2] - 3*x[6]*x[7] - 8*x[7]^2 - 3*x[6]*x[8] - 2*x[7]*x[8],
-    4*x[1]*x[3] - 8*x[5]^2 - x[7]*x[8] - x[8]^2,
-    24*x[1]*x[5] - 24*x[4]*x[5] - 24*x[5]^2 + 3*x[6]*x[7] - 10*x[7]^2 - 
-        3*x[7]*x[8],
-    18*x[1]*x[6] + 33*x[3]*x[6] - 60*x[4]*x[6] - 66*x[5]*x[6] + 20*x[4]*x[7] + 
-        88*x[5]*x[7] - 36*x[4]*x[8] - 38*x[5]*x[8],
-    3*x[3]*x[6] - 12*x[4]*x[6] - 24*x[5]*x[6] + 12*x[1]*x[7] - 32*x[4]*x[7] - 
-        40*x[5]*x[7] + 8*x[5]*x[8],
-    -3*x[3]*x[6] - 3*x[5]*x[6] - 8*x[5]*x[7] + 3*x[1]*x[8] - 2*x[5]*x[8],
-    3*x[2]^2 - 12*x[6]*x[8] - 8*x[7]*x[8],
-    x[2]*x[3] - x[8]^2,
-    4*x[2]*x[4] - 4*x[7]^2 + 4*x[7]*x[8] - x[8]^2,
-    x[2]*x[5] - x[7]*x[8],
-    9*x[2]*x[6] + 36*x[3]*x[6] - 48*x[5]*x[6] + 112*x[5]*x[7] - 72*x[4]*x[8] - 
-        48*x[5]*x[8],
-    -12*x[5]*x[6] + 3*x[2]*x[7] - 8*x[5]*x[7],
-    -12*x[3]*x[6] + 3*x[2]*x[8] - 8*x[5]*x[8],
-    6*x[3]^2 - 24*x[5]^2 - 3*x[6]*x[8] + 4*x[7]*x[8] - 3*x[8]^2,
-    24*x[3]*x[4] + 48*x[4]*x[5] + 6*x[6]*x[7] - 8*x[7]^2 - 3*x[6]*x[8] + 
-        10*x[7]*x[8] - 3*x[8]^2,
-    12*x[3]*x[5] - 24*x[4]*x[5] - 24*x[5]^2 - 3*x[6]*x[7] + 4*x[7]^2 - 
-        3*x[7]*x[8],
-    x[3]*x[7] - x[5]*x[8],
-    4*x[5]*x[7] + x[3]*x[8] - 4*x[4]*x[8] - 4*x[5]*x[8],
-    18*x[6]^2 - 12*x[6]*x[7] + 20*x[7]^2 + 18*x[6]*x[8] + 12*x[7]*x[8] - 
-        9*x[8]^2
+    2048*x[1]^2 + 16*x[3]^2 - x[5]*x[8],
+    512*x[1]*x[2] - x[5]*x[8],
+    16*x[1]*x[4] + 4*x[7]^2 + x[8]^2,
+    4*x[1]*x[5] - 4*x[3]*x[7] - x[4]*x[8],
+    512*x[1]*x[6] - x[5]*x[7],
+    x[3]*x[5] + 128*x[1]*x[7],
+    x[4]*x[5] + 128*x[1]*x[8],
+    128*x[2]^2 + 512*x[6]^2 - x[5]*x[8],
+    4*x[2]*x[3] + x[7]*x[8],
+    4*x[2]*x[4] + x[8]^2,
+    x[2]*x[5] - x[4]*x[8],
+    x[2]*x[7] - x[6]*x[8],
+    x[4]*x[5] + 128*x[6]*x[7] + 32*x[2]*x[8],
+    4*x[3]*x[4] + x[5]*x[7],
+    4*x[3]*x[6] + x[7]^2,
+    -x[5]*x[6] + x[3]*x[8],
+    4*x[4]^2 + x[5]*x[8],
+    4*x[4]*x[6] + x[7]*x[8],
+    -x[5]*x[6] + x[4]*x[7],
+    x[5]^2 + 128*x[7]^2 + 32*x[8]^2
 ]
 */
-#EllipticCurve(Curve(Reduction((l[1]),7))); //8
-#EllipticCurve(Curve(Reduction((l[2]),5))); //4
-#EllipticCurve(Curve(Reduction((l[3]),5))); //4
+#EllipticCurve(Curve(Reduction((l[1]),5))); //4
+#EllipticCurve(Curve(Reduction((l[2]),5))); //2
 
 Qx<x> := PolynomialRing(Rationals());
-E := EllipticCurve(x^3+x^2+x+1);
-#EllipticCurve(Curve(Reduction(E,5))); //8
-#EllipticCurve(Curve(Reduction(E,7))); //12
+E := EllipticCurve(x^3 - 2*x);
+#EllipticCurve(Curve(Reduction(E,5))); //10
+
+C1 := l[3];
+//Search for degree 2 divisors on C1
+P<[X]> := AmbientSpace(C1);
+deg := 2;
+tups := {};
+d := Dimension(P);
+ideals := {@@};
+tupsOld := tups;
+coeffs := [ 0, 1 ];
+    tups   :=
+        [ P![ tup[i] : i in [1..#tup] ] :
+          tup in CartesianPower(coeffs, d+1) |
+          not tup eq < 0 : i in [1..d+1] >  
+          and not P![tup[i] : i in [1..#tup]] in tupsOld
+        ];
+    for c in tups  do  
+            L := Scheme(P,&+[c[i]*X[i] : i in [1..Dimension(AmbientSpace(C1)) + 1]]);
+              irr := IrreducibleComponents(L meet C1);
+              degs := [Degree(ReducedSubscheme(i)) : i in irr];
+              if  deg in degs then
+        for cpt in irr do  
+         if  Degree(ReducedSubscheme(cpt)) eq deg
+             and not Basis(Ideal(ReducedSubscheme(cpt))) in ideals then      
+               ideals := ideals join {@Basis(Ideal(ReducedSubscheme(cpt)))@}; 
+        end if;
+    end for;
+    end if;
+    end for;
+    ideals;
