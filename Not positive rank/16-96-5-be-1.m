ffd88a63d5a55623fc1bb2ae34f4f15c7db90288
Mike C <mjcerchia@gmail.com>
2025-08-07 16:50:26 -0400

    Update 16-96-5-be-1.m

--- Not positive rank/16-96-5-be-1.m
+++ Not positive rank/16-96-5-be-1.m
@@ -1,4 +1,3 @@
-16.96.5.be.1
 /****************************************************************************** 
 Here is a summary of the argument.
 
@@ -21,7 +20,7 @@ end for;
 #auts eq #S;
 
 
-//There is one genus one quotients by an involution
+//There is one genus one quotient by an involution
 l := []; //list of genus 1 quotients by involutions
 m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't be in the same list.
 for g in auts do
@@ -41,7 +40,7 @@ print ".........";
 end for;
 
 l;
-\*
+/*
 [
     Curve over GF(5) defined by
     x[1]^2 + 3*x[1]*x[2] + x[2]^2 + 4*x[1]*x[3] + x[2]*x[3] + x[3]^2 + 
