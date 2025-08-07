164ba4028cc360f355d1971253f0c4aef795375f
Mike C <mjcerchia@gmail.com>
2025-08-07 16:47:04 -0400

    Update 16-192-9-bw-2.m

--- Not positive rank/16-192-9-bw-2.m
+++ Not positive rank/16-192-9-bw-2.m
@@ -31,7 +31,7 @@ end for;
 #auts eq #S;
 
 
-//There is one genus one quotients by an involution
+//There is one genus one quotient by an involution
 l := []; //list of genus 1 quotients by involutions
 m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't be in the same list.
 for g in auts do
@@ -51,7 +51,7 @@ print ".........";
 end for;
 
 l;
-\*
+/*
 [
     Curve over GF(3) defined by
     x[3]*x[5] + 2*x[2]*x[6],
