704eec5f90cd4384489af42602af07edd2436cda
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-12 16:39:18 -0400

    Create 16-96-3-bf-1.m

---
+++ Not positive rank/16-96-3-bf-1.m
@@ -0,0 +1,22 @@
+/* Summary : We compute the number of genus 1 quotients mod 5 and there are three. 
+We know that there are at least 3 quotients over Q, since we only have three quotients over F_5 we know these are all. Further, LMFDB gives all those quotients
+they are either pointless or rank 0*/
+
+
+P<x,y,z,w,t,u>:=ProjectiveSpace(Rationals(),5);
+C:=Curve(P,[x*y + y*u + w*t, x*y - x*z - y*u + z^2 - w*t, x*y + x*z - y*u - z^2 - w^2 + w*t + t^2, 4*y^2 + y*u + w^2, x*w - 4*y*t + w*u - t*u, x^2 - x*y - y*u + w^2 + w*t + 3*t^2 + 2*u^2, 2*x*w - x*t + 4*y*w - w*u - t*u]);
+p:=5;
+Cp:=Curve(Reduction(C,p));
+G:=AutomorphismGroup(Cp);
+S:=Automorphisms(Cp);
+for s in S do;
+s1:=G!s;
+if Order(s1) eq 2 then
+AG:=AutomorphismGroup(Cp,[s1]);
+CG:=CurveQuotient(AG);
+if Genus(CG) eq 1 then
+CG;
+end if;
+end if;
+print "........";
+end for;
