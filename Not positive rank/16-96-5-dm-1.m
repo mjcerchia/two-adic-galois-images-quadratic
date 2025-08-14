3fbe147462aafd546718f6e93056574241e5fa23
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-14 16:52:07 -0400

    Update 16-96-5-dm-1.m

--- Not positive rank/16-96-5-dm-1.m
+++ Not positive rank/16-96-5-dm-1.m
@@ -1,6 +1,5 @@
 /* Summary : We compute the number of genus 1 quotients mod 5 and there are two. 
-We know that there are at least 2 quotients over Q, since we only have two quotients over F_5 we know these are all. Further, LMFDB gives all those quotients
-they are either pointless or rank 0*/
+No quotient corresponds to 256.2.a.b*/
 
 P<x,y,z,w,t>:=ProjectiveSpace(Rationals(),4);
 C:=Curve(P,[y^2 + z^2 - z*w, 2*x*z + 2*x*w + t^2, 8*x^2 - z^2 + 2*w^2]);
@@ -15,7 +14,16 @@ AG:=AutomorphismGroup(Cp,[s1]);
 CG:=CurveQuotient(AG);
 if Genus(CG) eq 1 then
 CG;
+E:=EllipticCurve(CG);
+#Points(E); // 6 and 4
 end if;
 end if;
 print "........";
 end for;
+
+// For 256.2.a.b
+
+E := EllipticCurve([0, 0, 0, -2, 0]);
+Cp:=Curve(Reduction(E,5));
+Ep:=EllipticCurve(Cp);
+#Points(Ep); // 10
