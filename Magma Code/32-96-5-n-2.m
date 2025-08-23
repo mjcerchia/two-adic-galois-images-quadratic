e1174ba981cfc7f1b9a2a0fe51140259f7baf8e0
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-23 12:30:15 -0400

    Update 32-96-5-n-2.m

--- Magma Code/32-96-5-n-2.m
+++ Magma Code/32-96-5-n-2.m
@@ -16,7 +16,7 @@ Stemp := Automorphisms(C);
 for s in Stemp do
 auts := Append(auts, S!s);
 end for;
-#auts eq #S;
+assert #auts eq #S;
 
 //There are three genus one quotients by an involution
 l := []; //list of genus 1 quotients by involutions
@@ -24,7 +24,7 @@ m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't 
 for g in auts do
 if Order(g) eq 2 then
 AG := AutomorphismGroup(C,[g]);
-CG,prj := CurveQuotient(AG);prj;
+CG,prj := CurveQuotient(AG);
 if Genus(CG) eq 1 then
 try
 l := Append(l,CG);
@@ -32,38 +32,19 @@ catch e
 m := Append(m,CG);
 end try;
 end if;
-CG; Genus(CG);
+
 end if;
-print ".........";
+
 end for;
 
 //One of these quotients has the following model:
 P<[x]> := ProjectiveSpace(Rationals(),7);
-C1 := Curve(P,[2*x[1]^2 + 2*x[1]*x[3] + x[3]^2 - 4*x[5]*x[8] - 2*x[7]*x[8],
-4*x[1]*x[2] + 2*x[5]*x[8] + x[7]*x[8],
-8*x[1]*x[4] - 2*x[5]*x[7] - 5*x[7]^2 + 32*x[8]^2,
-8*x[1]*x[5] + 2*x[3]*x[5] - 5*x[3]*x[7] + 48*x[4]*x[8] + 24*x[6]*x[8],
-4*x[1]*x[6] + 2*x[5]*x[7] + x[7]^2,
-2*x[3]*x[5] + 4*x[1]*x[7] + 3*x[3]*x[7] - 16*x[4]*x[8] - 8*x[6]*x[8],
-4*x[4]*x[5] + 4*x[5]*x[6] + x[6]*x[7] + 4*x[1]*x[8],
-32*x[2]^2 - 4*x[6]^2 - 2*x[5]*x[8] - x[7]*x[8],
-4*x[2]*x[3] - 2*x[5]*x[8] - 3*x[7]*x[8],
-2*x[2]*x[4] + x[2]*x[6] - x[8]^2,
-x[2]*x[5] - x[4]*x[8],
-x[2]*x[7] - x[6]*x[8],
--4*x[4]*x[5] - 4*x[5]*x[6] - 5*x[6]*x[7] + 32*x[2]*x[8],
-8*x[3]*x[4] - 2*x[5]*x[7] + 5*x[7]^2 - 32*x[8]^2,
-4*x[3]*x[6] - 2*x[5]*x[7] - 3*x[7]^2,
--4*x[4]*x[5] - 8*x[5]*x[6] - 3*x[6]*x[7] + 4*x[3]*x[8],
-4*x[4]^2 - x[6]^2 - 2*x[5]*x[8] + x[7]*x[8],
-2*x[4]*x[6] + x[6]^2 - x[7]*x[8],
--x[5]*x[6] + x[4]*x[7],
-4*x[5]^2 + 4*x[5]*x[7] + 5*x[7]^2 - 32*x[8]^2]);
+C1 := l[2];
 
 //We can't immediately find a point, so we intersect with a hyperplane
-Points(C1 meet Scheme(AmbientSpace(C1),x[3]));
+
 
 //This produces a point
-pt := C1![-2,1/2,0,3/2,3,-1,-2,1]; 
+pt := Points(C1 meet Scheme(AmbientSpace(C1),x[3]))[1]; 
 E := EllipticCurve(C1,pt);
 Rank(E); //1
