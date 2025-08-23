761ba289ab34cbc7d3ca56dda82f9d6d753ed889
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-23 11:13:40 -0400

    Update 64-96-5-a-1.m

--- Magma Code/64-96-5-a-1.m
+++ Magma Code/64-96-5-a-1.m
@@ -17,7 +17,7 @@ Stemp := Automorphisms(C);
 for s in Stemp do
 auts := Append(auts, S!s);
 end for;
-#auts eq #S;
+assert #auts eq #S;
 
 //There are three genus one quotients by an involution
 l := []; //list of genus 1 quotients by involutions
@@ -25,7 +25,7 @@ m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't 
 for g in auts do
 if Order(g) eq 2 then
 AG := AutomorphismGroup(C,[g]);
-CG,prj := CurveQuotient(AG);prj;
+CG,prj := CurveQuotient(AG);
 if Genus(CG) eq 1 then
 try
 l := Append(l,CG);
@@ -33,33 +33,14 @@ catch e
 m := Append(m,CG);
 end try;
 end if;
-CG; Genus(CG);
+
 end if;
-print ".........";
+
 end for;
 
 //One of these quotients has the following model:
 P<[x]> := ProjectiveSpace(Rationals(),7);
-C1 := Curve(P,[50*x[1]^2 + x[3]^2 - 25*x[7]^2,
-x[1]*x[2] - x[7]^2,
-x[1]*x[4] - x[3]*x[6],
-x[1]*x[5] - x[4]*x[6],
-x[3]*x[4] + 50*x[1]*x[6] - 25*x[7]*x[8],
--x[6]^2 + x[1]*x[7],
--x[6]*x[7] + x[1]*x[8],
-25*x[2]^2 - x[5]^2 - 50*x[7]^2,
-x[2]*x[3] - x[5]*x[7],
-x[2]*x[4] - x[5]*x[8],
-x[2]*x[6] - x[7]*x[8],
-x[2]*x[7] - x[8]^2,
--x[4]*x[5] - 50*x[6]*x[7] + 25*x[2]*x[8],
-x[3]*x[5] + 50*x[6]^2 - 25*x[8]^2,
--x[4]*x[6] + x[3]*x[7],
--x[5]*x[6] + x[3]*x[8],
-x[4]^2 + 50*x[6]^2 - 25*x[8]^2,
--x[5]*x[6] + x[4]*x[7],
--x[5]*x[7] + x[4]*x[8],
--x[7]^2 + x[6]*x[8] ]);
+C1 := l[2];
 
 //We can't immediately find a point, so we intersect with hyperplanes to find a point
 P<[X]> := AmbientSpace(C1);
