d2dc9bf805d4a161988a0cdbc0a024befda99e98
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-23 12:40:49 -0400

    Update 32-96-5-i-1.m

--- Magma Code/32-96-5-i-1.m
+++ Magma Code/32-96-5-i-1.m
@@ -15,7 +15,7 @@ Stemp := Automorphisms(C);
 for s in Stemp do
 auts := Append(auts, S!s);
 end for;
-#auts eq #S;
+assert #auts eq #S;
 
 //There are three genus one quotients by an involution
 l := []; //list of genus 1 quotients by involutions
@@ -23,7 +23,7 @@ m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't 
 for g in auts do
 if Order(g) eq 2 then
 AG := AutomorphismGroup(C,[g]);
-CG,prj := CurveQuotient(AG);prj;
+CG,prj := CurveQuotient(AG);
 if Genus(CG) eq 1 then
 try
 l := Append(l,CG);
@@ -31,33 +31,14 @@ catch e
 m := Append(m,CG);
 end try;
 end if;
-CG; Genus(CG);
+
 end if;
-print ".........";
+
 end for;
 
 //One of these quotients has the following model:
 P<[x]> := ProjectiveSpace(Rationals(),7);
-C1 := Curve(P,[x[1]^2 - x[3]^2 + x[7]^2 - x[7]*x[8],
-4*x[1]*x[2] - x[7]*x[8],
-4*x[1]*x[4] - x[5]*x[7],
-x[1]*x[5] - x[3]*x[7],
-4*x[1]*x[6] - x[7]^2,
--x[3]*x[5] + x[1]*x[7] + 4*x[6]*x[7] - 4*x[6]*x[8],
--x[6]*x[7] + x[1]*x[8],
-16*x[2]^2 + 4*x[4]^2 - x[7]*x[8] - 4*x[8]^2,
-4*x[2]*x[3] - x[5]*x[8],
-x[2]*x[5] - x[4]*x[8],
-4*x[2]*x[6] - x[8]^2,
-x[2]*x[7] - x[6]*x[8],
-x[4]*x[5] - x[6]*x[7] + 4*x[2]*x[8] - 4*x[6]*x[8],
-4*x[3]*x[4] - x[7]^2 - 4*x[7]*x[8] + 4*x[8]^2,
-4*x[3]*x[6] - x[5]*x[7],
--x[5]*x[6] + x[3]*x[8],
-4*x[4]*x[6] - x[5]*x[8],
--x[5]*x[6] + x[4]*x[7],
-x[5]^2 - x[7]^2 - 4*x[7]*x[8] + 4*x[8]^2,
-4*x[6]^2 - x[7]*x[8]]);
+C1 := l[2];
 
 //We can find a point
   rationalPoints := function(D : Bound := 1)
@@ -69,7 +50,6 @@ x[5]^2 - x[7]^2 - 4*x[7]*x[8] + 4*x[8]^2,
 DefiningEquations(D)} eq {0}
             @};
   end function;
-  rationalPoints(C1:Bound := 1);
 pt :=   rationalPoints(C1:Bound := 1)[1];
 
 E := EllipticCurve(C1,pt);
