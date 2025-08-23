870a5612c941681217e01e621d9ecf3e43db682b
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-23 16:24:15 -0400

    Update 16-96-3-dw-1.m

--- Magma Code/16-96-3-dw-1.m
+++ Magma Code/16-96-3-dw-1.m
@@ -6,9 +6,7 @@ We find there are five genus one quotients by an involution. We construct a simp
 on one of these quotients and find that it is a rank 1 elliptic curve. 
 ******************************************************************************/
 P<x,y,z,u,t,w>:=ProjectiveSpace(Rationals(),5);
- C := Curve(P,[x^2 - y*z, 2*y*w + 2*w^2 - t*u + u^2, 2*y*w - 2*w^2 - t^2 - t*\
-u, y*w + 4*z*w - t*u, -y*u + 4*z*t - w*t + w*u, y*t + 4*z*u + w*t + w*u, y^2 +\
- 16*z^2 - 2*w^2]);
+ C := Curve(P,[x^2 - y*z, 2*y*w + 2*w^2 - t*u + u^2, 2*y*w - 2*w^2 - t^2 - t*u, y*w + 4*z*w - t*u, -y*u + 4*z*t - w*t + w*u, y*t + 4*z*u + w*t + w*u, y^2 + 16*z^2 - 2*w^2]);
 
 S := AutomorphismGroup(C); 
 auts := [];
@@ -16,7 +14,7 @@ Stemp := Automorphisms(C);
 for s in Stemp do
 auts := Append(auts, S!s);
 end for;
-#auts eq #S;
+assert #auts eq #S;
 
 //There are five genus one quotients by an involution
 l := []; //list of genus 1 quotients by involutions
@@ -24,7 +22,7 @@ m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't 
 for g in auts do
 if Order(g) eq 2 then
 AG := AutomorphismGroup(C,[g]);
-CG,prj := CurveQuotient(AG);prj;
+CG,prj := CurveQuotient(AG);
 if Genus(CG) eq 1 then
 try
 l := Append(l,CG);
@@ -32,31 +30,12 @@ catch e
 m := Append(m,CG);
 end try;
 end if;
-CG; Genus(CG);
+
 end if;
-print ".........";
+
 end for;
 
-l;
-/*
-[
-    Curve over Rational Field defined by
-    x[2]^2 - x[1]*x[3],
-    -16*x[1]^2 - 32*x[1]*x[2] - 40*x[2]^2 - 24*x[2]*x[3] - 7*x[3]^2 + x[4]^2,
-    Curve over Rational Field defined by
-    x[2]^2 - x[1]*x[3],
-    16*x[1]^2 + 32*x[2]^2 + 8*x[3]^2 + x[4]^2,
-    Curve over Rational Field defined by
-    x[2]^2 - x[1]*x[3],
-    8*x[1]^2 + 32*x[2]^2 + 16*x[3]^2 + x[4]^2,
-    Curve over Rational Field defined by
-    x[2]^2 - x[1]*x[3],
-    16*x[1]^2 - 96*x[2]^2 + 16*x[3]^2 + x[4]^2,
-    Curve over Rational Field defined by
-    x[2]^2 - x[1]*x[3],
-    -16*x[1]^2 - 32*x[1]*x[2] - 40*x[2]^2 - 24*x[2]*x[3] - 7*x[3]^2 + x[4]^2
-]
-*/
+
 
 //One of these quotients has the following model:
 
