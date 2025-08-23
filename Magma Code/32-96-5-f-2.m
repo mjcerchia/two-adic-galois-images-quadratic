e0ee0d5fc4dfa32fad6a20d95feaf7d1d195516e
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-23 12:50:06 -0400

    Update 32-96-5-f-2.m

--- Magma Code/32-96-5-f-2.m
+++ Magma Code/32-96-5-f-2.m
@@ -18,7 +18,7 @@ Stemp := Automorphisms(C);
 for s in Stemp do
 auts := Append(auts, S!s);
 end for;
-#auts eq #S;
+assert #auts eq #S;
 
 //We find three genus one quotients by involution
 l := []; //list of genus 1 quotients by involutions
@@ -26,7 +26,7 @@ m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't 
 for g in auts do
 if Order(g) eq 2 then
 AG := AutomorphismGroup(C,[g]);
-CG,prj := CurveQuotient(AG);prj;
+CG,prj := CurveQuotient(AG);
 if Genus(CG) eq 1 then
 try
 l := Append(l,CG);
@@ -34,37 +34,17 @@ catch e
 m := Append(m,CG);
 end try;
 end if;
-CG; Genus(CG);
+
 end if;
-print ".........";
+
 end for;
 
 P<[x]> := ProjectiveSpace(Rationals(),7);
-C1 :=Curve(P,[16*x[1]^2 - x[3]^2 - 2*x[7]^2,
-x[1]*x[2] - x[7]^2,
-x[1]*x[4] - x[3]*x[6],
-x[1]*x[5] - x[4]*x[6],
--x[3]*x[4] + 16*x[1]*x[6] - 2*x[7]*x[8],
--x[6]^2 + x[1]*x[7],
--x[6]*x[7] + x[1]*x[8],
-2*x[2]^2 + x[5]^2 - 16*x[7]^2,
-x[2]*x[3] - x[5]*x[7],
-x[2]*x[4] - x[5]*x[8],
-x[2]*x[6] - x[7]*x[8],
-x[2]*x[7] - x[8]^2,
-x[4]*x[5] - 16*x[6]*x[7] + 2*x[2]*x[8],
-x[3]*x[5] - 16*x[6]^2 + 2*x[8]^2,
--x[4]*x[6] + x[3]*x[7],
--x[5]*x[6] + x[3]*x[8],
-x[4]^2 - 16*x[6]^2 + 2*x[8]^2,
--x[5]*x[6] + x[4]*x[7],
--x[5]*x[7] + x[4]*x[8],
--x[7]^2 + x[6]*x[8]]);
+C1 :=l[3];
 
 //By intersecting with a hyperplane, we find a point
-Points(C1 meet Scheme(AmbientSpace(C1),x[2]));
 
 //This produces a point
-pt:=C1![-1/4 , 0 , 1 , 0 , 0 , 0 , 0 , 0];
+pt:=Points(C1 meet Scheme(AmbientSpace(C1),x[2]))[1];
 E := EllipticCurve(C1,pt);
 Rank(E); //1
