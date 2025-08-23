0fbc4a9ef69a50d2926fc34fc55f9f5e8ff89cf6
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-23 16:01:06 -0400

    Update 16-96-5-s-1.m

--- Magma Code/16-96-5-s-1.m
+++ Magma Code/16-96-5-s-1.m
@@ -2,8 +2,8 @@
 Here is a summary of the argument.
 
 Let C be the modular curve with lmfdb label 16.96.5.s.1
-We compute the automorphism group over Q and find there are three genus one quotients by an involution. The first and third have 
-4 and 2 points, respectively, mod 3. For the second, we construct a new model and find that it is rank one. 
+We compute the automorphism group over Q and find there are three genus one quotients by an involution. Two quotients have 
+4 and 2 points, respectively, mod 3 which does not match with points of rank 1 newform factor in Jacobian. For the third, we construct a new model and find that it is rank one. 
 ******************************************************************************/
 P<x,y,z,w,t> := ProjectiveSpace(Rationals(),4);
 C := Curve(P,[2*x^2 + y*z, 2*y*z + w*t, 2*y^2 + 2*z^2 + w^2 - t^2]);
@@ -14,7 +14,7 @@ Stemp := Automorphisms(C);
 for s in Stemp do
 auts := Append(auts, S!s);
 end for;
-#auts eq #S;
+assert #auts eq #S;
 
 
 //There are three genus one quotients by an involution
@@ -23,7 +23,7 @@ m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't 
 for g in auts do
 if Order(g) eq 2 then
 AG := AutomorphismGroup(C,[g]);
-CG,prj := CurveQuotient(AG);prj;
+CG,prj := CurveQuotient(AG);
 if Genus(CG) eq 1 then
 try
 l := Append(l,CG);
@@ -31,89 +31,24 @@ catch e
 m := Append(m,CG);
 end try;
 end if;
-CG; Genus(CG);
+
 end if;
-print ".........";
+
 end for;
 
-l;
 
-/*
-[
-    Curve over Rational Field defined by
-    x[1]^2 + 2*x[4]^2 + 16*x[6]^2 - 8*x[7]^2,
-    x[1]*x[2] - x[6]^2,
-    x[1]*x[3] - x[7]^2,
-    x[1]*x[5] - x[4]*x[6],
-    x[1]*x[6] - x[4]*x[7],
-    2*x[4]*x[6] + x[1]*x[7] - 8*x[3]*x[7] + 16*x[6]*x[8],
-    -x[6]*x[7] + x[1]*x[8],
-    16*x[2]^2 + 2*x[5]^2 + x[6]^2 - 8*x[8]^2,
-    x[2]*x[3] - x[8]^2,
-    x[2]*x[4] - x[5]*x[6],
-    x[2]*x[6] - x[5]*x[8],
-    x[2]*x[7] - x[6]*x[8],
-    2*x[5]*x[6] + x[6]*x[7] + 16*x[2]*x[8] - 8*x[3]*x[8],
-    8*x[3]^2 - 2*x[6]^2 - x[7]^2 - 16*x[8]^2,
-    x[3]*x[4] - x[6]*x[7],
-    x[3]*x[5] - x[6]*x[8],
-    x[3]*x[6] - x[7]*x[8],
-    2*x[4]*x[5] + x[4]*x[7] + 16*x[5]*x[8] - 8*x[7]*x[8],
-    -x[6]^2 + x[4]*x[8],
-    -x[6]^2 + x[5]*x[7],
-    Curve over Rational Field defined by
-    x[1]^2 + 2*x[3]^2 - 8*x[6]^2 + x[7]^2 - 8*x[5]*x[8],
-    x[1]*x[2] - x[6]^2 + x[8]^2,
-    8*x[1]*x[4] - x[7]^2 + 8*x[5]*x[8],
-    x[1]*x[5] + 8*x[4]*x[5] - x[3]*x[7],
-    4*x[1]*x[6] - 32*x[2]*x[6] + x[5]*x[7],
-    2*x[3]*x[5] + x[1]*x[7] - 8*x[6]*x[8],
-    -x[6]*x[7] + x[1]*x[8] + 8*x[4]*x[8],
-    32*x[2]^2 - 4*x[6]^2 - x[5]*x[8] + 4*x[8]^2,
-    8*x[2]*x[3] - x[7]*x[8],
-    8*x[2]*x[4] - x[8]^2,
-    x[2]*x[5] - x[4]*x[8],
-    x[2]*x[7] - x[6]*x[8],
-    -2*x[4]*x[5] - x[6]*x[7] + 8*x[2]*x[8] + 8*x[4]*x[8],
-    8*x[3]*x[4] - x[5]*x[7],
-    8*x[3]*x[6] - x[7]^2,
-    -x[5]*x[6] + x[3]*x[8],
-    8*x[4]^2 - x[5]*x[8],
-    8*x[4]*x[6] - x[7]*x[8],
-    -x[5]*x[6] + x[4]*x[7],
-    2*x[5]^2 + x[7]^2 - 8*x[5]*x[8] - 8*x[8]^2,
-    Curve over Rational Field defined by
-    x[1]^2 + 2*x[3]^2 - 8*x[6]^2 - x[7]^2 - 8*x[5]*x[8],
-    x[1]*x[2] - x[6]^2 - x[8]^2,
-    8*x[1]*x[4] - x[7]^2 - 8*x[5]*x[8],
-    x[1]*x[5] - 8*x[4]*x[5] - x[3]*x[7],
-    4*x[1]*x[6] - 32*x[2]*x[6] + x[5]*x[7],
-    2*x[3]*x[5] + x[1]*x[7] - 8*x[6]*x[8],
-    -x[6]*x[7] + x[1]*x[8] - 8*x[4]*x[8],
-    32*x[2]^2 - 4*x[6]^2 - x[5]*x[8] - 4*x[8]^2,
-    8*x[2]*x[3] - x[7]*x[8],
-    8*x[2]*x[4] - x[8]^2,
-    x[2]*x[5] - x[4]*x[8],
-    x[2]*x[7] - x[6]*x[8],
-    -2*x[4]*x[5] - x[6]*x[7] + 8*x[2]*x[8] - 8*x[4]*x[8],
-    8*x[3]*x[4] - x[5]*x[7],
-    8*x[3]*x[6] - x[7]^2,
-    -x[5]*x[6] + x[3]*x[8],
-    8*x[4]^2 - x[5]*x[8],
-    8*x[4]*x[6] - x[7]*x[8],
-    -x[5]*x[6] + x[4]*x[7],
-    2*x[5]^2 + x[7]^2 + 8*x[5]*x[8] - 8*x[8]^2
-]
-*/
+
 #EllipticCurve(Curve(Reduction(l[1],3))); //4
 #EllipticCurve(Curve(Reduction(l[3],3))); //2
 
 Qx<x> := PolynomialRing(Rationals());
 E := EllipticCurve(x^3+x^2+x+1);
-#EllipticCurve(Curve(Reduction(E,3))) //6
+#EllipticCurve(Curve(Reduction(E,3))); //6
+
+/*********************************************************************************************************/
 
 //Search for degree two divisors
-C1 := l[3];
+C1 := l[2];
 P<[X]> := AmbientSpace(C1);
 deg := 2;
 tups := {};
@@ -172,7 +107,7 @@ P<x>:=PolynomialRing(Rationals());
 f := -(-512*x^4 - 64*x^2 + 2);  
 H := HyperellipticCurve(f);
 
-HasPointsEverywhereLocally(f,2); 
+
 RationalPoints(H : Bound := 10000);
 /*
 {@ (-3 : -224 : 4), (-3 : 224 : 4), (-1 : -32 : 4), (-1 : 32 : 4), (1 : -32 : 
