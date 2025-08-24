0e91904b83e7f56088a67b97d189be027b27e55d
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-24 16:24:16 -0400

    Update 16-96-5-be-1.m

--- Not positive rank/16-96-5-be-1.m
+++ Not positive rank/16-96-5-be-1.m
@@ -7,8 +7,7 @@ The single rank one elliptic curve factor of the Jacobian has 8 points over F5.
 NOT bielliptic. 
 ******************************************************************************/
 P<x,y,z,w,t>:=ProjectiveSpace(Rationals(),4);
- C := Curve(P,[y^2 - z^2 - w^2 + t^2, y*z - y*w - y*t + z^2 + z*w + z*t + w*t\
- - t^2, 8*x^2 + y*t - z^2 - z*w + t^2]);
+ C := Curve(P,[y^2 - z^2 - w^2 + t^2, y*z - y*w - y*t + z^2 + z*w + z*t + w*t - t^2, 8*x^2 + y*t - z^2 - z*w + t^2]);
  C5 := Curve(Reduction(C,5)); 
 S := AutomorphismGroup(C5); 
 
@@ -17,7 +16,7 @@ Stemp := Automorphisms(C5);
 for s in Stemp do
 auts := Append(auts, S!s);
 end for;
-#auts eq #S;
+assert #auts eq #S;
 
 
 //There is one genus one quotient by an involution
@@ -26,7 +25,7 @@ m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't 
 for g in auts do
 if Order(g) eq 2 then
 AG := AutomorphismGroup(C5,[g]);
-CG,prj := CurveQuotient(AG);prj;
+CG,prj := CurveQuotient(AG);
 if Genus(CG) eq 1 then
 try
 l := Append(l,CG);
@@ -34,56 +33,13 @@ catch e
 m := Append(m,CG);
 end try;
 end if;
-CG; Genus(CG);
+
 end if;
-print ".........";
+
 end for;
 
-l;
-/*
-[
-    Curve over GF(5) defined by
-    x[1]^2 + 3*x[1]*x[2] + x[2]^2 + 4*x[1]*x[3] + x[2]*x[3] + x[3]^2 + 
-        2*x[2]*x[4] + 3*x[2]*x[5] + 3*x[3]*x[5] + 4*x[2]*x[6] + 2*x[1]*x[7] + 
-        2*x[2]*x[7] + 3*x[1]*x[8] + 2*x[2]*x[8],
-    4*x[1]^2 + 2*x[2]^2 + 3*x[1]*x[3] + 2*x[2]*x[3] + x[3]^2 + 3*x[1]*x[4] + 
-        x[2]*x[4] + 3*x[1]*x[5] + 3*x[2]*x[5] + x[2]*x[6] + 3*x[3]*x[6] + 
-        x[1]*x[7] + 2*x[2]*x[7] + x[1]*x[8] + 3*x[2]*x[8],
-    x[1]^2 + 4*x[1]*x[2] + 3*x[2]^2 + 4*x[1]*x[3] + 3*x[2]*x[3] + x[3]^2 + 
-        4*x[2]*x[4] + 4*x[1]*x[5] + 2*x[2]*x[5] + 2*x[2]*x[6] + 4*x[2]*x[7] + 
-        x[3]*x[7] + x[1]*x[8] + x[2]*x[8],
-    x[1]^2 + 2*x[1]*x[2] + 2*x[2]^2 + 4*x[1]*x[3] + 2*x[2]*x[3] + x[3]^2 + 
-        4*x[2]*x[4] + 3*x[1]*x[5] + 3*x[2]*x[5] + 2*x[2]*x[7] + 2*x[3]*x[8],
-    2*x[1]^2 + x[1]*x[3] + 3*x[1]*x[4] + 2*x[4]^2 + 4*x[1]*x[5] + x[1]*x[6] + 
-        4*x[1]*x[7] + x[1]*x[8],
-    x[1]^2 + 2*x[2]^2 + 4*x[1]*x[3] + 2*x[2]*x[3] + x[3]^2 + x[2]*x[4] + 
-        2*x[1]*x[5] + 3*x[2]*x[5] + x[4]*x[5] + 2*x[2]*x[6] + 3*x[1]*x[7] + 
-        2*x[2]*x[7] + x[1]*x[8],
-    2*x[1]*x[3] + 3*x[4]*x[6],
-    x[1]^2 + 2*x[1]*x[2] + 2*x[2]^2 + 4*x[1]*x[3] + 2*x[2]*x[3] + x[3]^2 + 
-        4*x[2]*x[4] + 3*x[1]*x[5] + 3*x[2]*x[5] + 2*x[2]*x[7] + 2*x[4]*x[7],
-    3*x[1]*x[5] + 2*x[4]*x[8],
-    2*x[1]*x[2] + x[2]*x[3] + 3*x[2]*x[4] + 4*x[2]*x[5] + 2*x[5]^2 + x[2]*x[6] +
-        4*x[2]*x[7] + x[2]*x[8],
-    x[1]^2 + 2*x[1]*x[2] + 2*x[2]^2 + 4*x[1]*x[3] + 2*x[2]*x[3] + x[3]^2 + 
-        4*x[2]*x[4] + 3*x[1]*x[5] + 3*x[2]*x[5] + 2*x[5]*x[6] + 2*x[2]*x[7],
-    3*x[2]*x[3] + 2*x[5]*x[7],
-    x[2]*x[4] + 4*x[5]*x[8],
-    2*x[1]^2 + 2*x[1]*x[2] + 3*x[1]*x[3] + x[1]*x[4] + 4*x[1]*x[5] + 4*x[1]*x[6]
-        + x[6]^2 + 2*x[1]*x[7] + x[1]*x[8],
-    x[1]^2 + 3*x[1]*x[2] + 2*x[2]^2 + 4*x[1]*x[3] + 2*x[2]*x[3] + x[3]^2 + 
-        3*x[2]*x[4] + 4*x[1]*x[5] + 3*x[2]*x[5] + 2*x[2]*x[6] + 4*x[1]*x[7] + 
-        2*x[2]*x[7] + x[6]*x[7] + 2*x[1]*x[8] + 2*x[2]*x[8],
-    3*x[1]*x[7] + 2*x[6]*x[8],
-    2*x[1]*x[2] + 2*x[2]^2 + 3*x[2]*x[3] + x[2]*x[4] + 4*x[2]*x[5] + 4*x[2]*x[6]
-        + 2*x[2]*x[7] + x[7]^2 + x[2]*x[8],
-    3*x[2]*x[6] + 2*x[7]*x[8],
-    2*x[1]*x[2] + 3*x[8]^2,
-    2*x[1]^2 + x[1]*x[2] + 2*x[2]^2 + 2*x[2]*x[3] + x[3]^2 + 4*x[1]*x[4] + 
-        4*x[2]*x[4] + x[3]*x[4] + 4*x[1]*x[5] + 3*x[2]*x[5] + 4*x[1]*x[6] + 
-        3*x[1]*x[7] + 2*x[2]*x[7] + 2*x[1]*x[8] + 3*x[2]*x[8]
-]
-*/
+#l;
+
 #EllipticCurve(Curve(l[1])); //4
 
 Qx<x> := PolynomialRing(Rationals());
