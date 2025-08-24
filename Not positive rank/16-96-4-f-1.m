fca69515f819f63ce71028f0a55f096aa0845807
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-24 16:21:29 -0400

    Update 16-96-4-f-1.m

--- Not positive rank/16-96-4-f-1.m
+++ Not positive rank/16-96-4-f-1.m
@@ -14,7 +14,7 @@ Stemp := Automorphisms(C);
 for s in Stemp do
 auts := Append(auts, S!s);
 end for;
-#auts eq #S;
+assert #auts eq #S;
 
 //There are two genus one quotients by an involution
 l := []; //list of genus 1 quotients by involutions
@@ -22,7 +22,7 @@ m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't 
 for g in auts do
 if Order(g) eq 2 then
 AG := AutomorphismGroup(C,[g]);
-CG,prj := CurveQuotient(AG);prj;
+CG,prj := CurveQuotient(AG);
 if Genus(CG) eq 1 then
 try
 l := Append(l,CG);
@@ -30,36 +30,14 @@ catch e
 m := Append(m,CG);
 end try;
 end if;
-CG; Genus(CG);
+
 end if;
-print ".........";
+
 end for;
 
-l;
-/*
-  [
-    Curve over Rational Field defined by
-    2*x[1]^2 + 4*x[2]*x[4] - 10*x[4]^2 - x[5]^2 + x[5]*x[6] + 2*x[6]^2,
-    x[1]*x[2] - x[4]^2,
-    x[1]*x[3] - x[5]^2,
-    2*x[1]*x[4] - 2*x[2]*x[4] + 4*x[4]^2 - x[5]*x[6] - x[6]^2,
-    2*x[1]*x[5] - x[3]*x[5] + 4*x[4]*x[5] - x[3]*x[6] - 2*x[4]*x[6],
-    -x[4]*x[5] + x[1]*x[6],
-    x[2]*x[3] - x[6]^2,
-    x[2]*x[5] - x[4]*x[6],
-    x[3]*x[4] - x[5]*x[6],
-    Curve over Rational Field defined by
-    x[1]*x[2] - x[4]^2,
-    x[1]*x[3] - x[5]^2,
-    x[1]*x[4] - x[2]*x[4] - 2*x[4]^2 + 8*x[5]^2 + 8*x[5]*x[6],
-    -x[4]*x[5] + x[1]*x[6],
-    x[2]^2 + 2*x[2]*x[4] - x[4]^2 - 8*x[5]*x[6] - 8*x[6]^2,
-    x[2]*x[3] - x[6]^2,
-    x[2]*x[5] - x[4]*x[6],
-    -8*x[3]*x[5] - x[4]*x[5] + x[2]*x[6] - 8*x[3]*x[6] + 2*x[4]*x[6],
-    x[3]*x[4] - x[5]*x[6]
-]
-*/
+#l;
+
+
 
 C1 := l[1];
   rationalPoints := function(D : Bound := 1)
@@ -71,13 +49,13 @@ C1 := l[1];
 DefiningEquations(D)} eq {0}
             @};
   end function;
-  rationalPoints(C1:Bound := 1);
+  
 pt :=   rationalPoints(C1:Bound := 1)[1];
 
 E:=EllipticCurve(C1,pt);
 Rank(E); // 0
 
-C1 := l[2];
+C2 := l[2];
   rationalPoints := function(D : Bound := 1)
     return {@D![t :t in tup]
               : tup in CartesianPower([-Bound..Bound],Dimension(AmbientSpace(D)\
@@ -87,8 +65,8 @@ C1 := l[2];
 DefiningEquations(D)} eq {0}
             @};
   end function;
-  rationalPoints(C1:Bound := 1);
-pt :=   rationalPoints(C1:Bound := 1)[1];
+  
+pt :=   rationalPoints(C2:Bound := 1)[1];
 
-E:=EllipticCurve(C1,pt);
+E:=EllipticCurve(C2,pt);
 Rank(E); //0
