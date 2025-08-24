8f45bbb52ee49a6b64b628a4bbeec7c3fac0bbcb
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-24 10:26:48 -0400

    Update 11-165-5-a-1.m

--- Not positive rank/11-165-5-a-1.m
+++ Not positive rank/11-165-5-a-1.m
@@ -6,10 +6,7 @@ We compute the automorphism group over F5 and find there are no genus one quotie
 NOT bielliptic. 
 ******************************************************************************/
 P<x,y,z,w,t>:=ProjectiveSpace(Rationals(),4);
-C := Curve(P,[3*x^2 + 3*x*y - x*z - 3*x*w + 3*x*t + 3*y^2 + 2*y*z - y*w - 2*\
-z^2 - 2*z*w + 2*z*t + w^2, 2*x*y - 2*x*z + 6*x*w - x*t - 3*y^2 + 3*y*z - y*t +\
- 2*z^2 - 7*z*w - z*t - t^2, 8*x^2 - 10*x*y + 5*x*z - 3*x*w - x*t - 3*y^2 + 3*y\
-*z + 4*y*w + 3*y*t - 3*z^2 - 7*z*w - z*t + 3*w^2 + 3*w*t - t^2]);
+C := Curve(P,[3*x^2 + 3*x*y - x*z - 3*x*w + 3*x*t + 3*y^2 + 2*y*z - y*w - 2*z^2 - 2*z*w + 2*z*t + w^2, 2*x*y - 2*x*z + 6*x*w - x*t - 3*y^2 + 3*y*z - y*t + 2*z^2 - 7*z*w - z*t - t^2, 8*x^2 - 10*x*y + 5*x*z - 3*x*w - x*t - 3*y^2 + 3*y*z + 4*y*w + 3*y*t - 3*z^2 - 7*z*w - z*t + 3*w^2 + 3*w*t - t^2]);
 
 C5 := Curve(Reduction(C,5));
 S := AutomorphismGroup(C5); 
@@ -19,7 +16,7 @@ Stemp := Automorphisms(C5);
 for s in Stemp do
 auts := Append(auts, S!s);
 end for;
-#auts eq #S;
+assert #auts eq #S;
 
 
 //There are no genus one quotients by an involution
@@ -28,7 +25,7 @@ m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't 
 for g in auts do
 if Order(g) eq 2 then
 AG := AutomorphismGroup(C5,[g]);
-CG,prj := CurveQuotient(AG);prj;
+CG,prj := CurveQuotient(AG);
 if Genus(CG) eq 1 then
 try
 l := Append(l,CG);
@@ -36,10 +33,9 @@ catch e
 m := Append(m,CG);
 end try;
 end if;
-CG; Genus(CG);
+
 end if;
-print ".........";
+
 end for;
 
 l;
-
