f4560bf42d7f5136f3d0bb723839e03112c55089
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-24 20:29:22 -0400

    Update 16-96-5-cv-1.m

--- Not positive rank/16-96-5-cv-1.m
+++ Not positive rank/16-96-5-cv-1.m
@@ -2,7 +2,7 @@
 Here is a summary of the argument.
 
 Let C be the modular curve with lmfdb label 16.96.5.cv.1
-We compute the automorphism group over F5 and find there are two genus one quotient by an involution. They have 4 and 6 points.
+We compute the automorphism group over F5 and find there are two genus one quotient by an involution. They have 8 and 6 points.
 The single rank one elliptic curve factor of the Jacobian has 10 points over F5.
 NOT bielliptic. 
 ******************************************************************************/
@@ -16,7 +16,7 @@ Stemp := Automorphisms(C5);
 for s in Stemp do
 auts := Append(auts, S!s);
 end for;
-#auts eq #S;
+assert #auts eq #S;
 
 
 //There are two genus one quotients by an involution
@@ -25,7 +25,7 @@ m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't 
 for g in auts do
 if Order(g) eq 2 then
 AG := AutomorphismGroup(C5,[g]);
-CG,prj := CurveQuotient(AG);prj;
+CG,prj := CurveQuotient(AG);
 if Genus(CG) eq 1 then
 try
 l := Append(l,CG);
@@ -33,72 +33,14 @@ catch e
 m := Append(m,CG);
 end try;
 end if;
-CG; Genus(CG);
+
 end if;
-print ".........";
+
 end for;
 
-l;
-/*
-[
-    Curve over GF(5) defined by
-    3*x[1]*x[2] + 2*x[2]^2 + 2*x[2]*x[3] + x[2]*x[4] + 2*x[3]*x[5] + 4*x[2]*x[6]
-        + x[5]*x[6] + 3*x[2]*x[7] + 2*x[2]*x[8],
-    x[1]*x[2] + 2*x[1]*x[3] + 4*x[2]*x[4] + 2*x[1]*x[5] + x[2]*x[6] + 
-        2*x[3]*x[6] + 3*x[5]*x[6] + 4*x[1]*x[7] + 4*x[1]*x[8] + 2*x[2]*x[8],
-    3*x[1]*x[2] + 4*x[2]^2 + x[2]*x[3] + 4*x[2]*x[4] + 3*x[2]*x[5] + 3*x[2]*x[6]
-        + 4*x[5]*x[6] + 2*x[2]*x[7] + 4*x[3]*x[7] + 2*x[2]*x[8],
-    x[5]*x[6] + 4*x[3]*x[8],
-    x[1]^2 + 3*x[1]*x[2] + 2*x[1]*x[3] + x[1]*x[4] + 3*x[4]^2 + x[1]*x[5] + 
-        x[1]*x[6] + 4*x[1]*x[7] + 4*x[1]*x[8],
-    x[1]*x[2] + 4*x[2]*x[4] + 4*x[1]*x[5] + 2*x[4]*x[5] + x[2]*x[6] + 
-        3*x[5]*x[6] + 4*x[1]*x[7] + 4*x[1]*x[8] + 2*x[2]*x[8],
-    2*x[1]*x[3] + 3*x[4]*x[6],
-    x[5]*x[6] + 4*x[4]*x[7],
-    x[1]*x[5] + 4*x[4]*x[8],
-    3*x[1]*x[2] + 4*x[2]^2 + x[2]*x[3] + 3*x[2]*x[4] + 3*x[2]*x[5] + 4*x[5]^2 + 
-        3*x[2]*x[6] + 2*x[2]*x[7] + 2*x[2]*x[8],
-    2*x[1]*x[2] + x[1]*x[3] + 2*x[3]*x[4] + x[1]*x[5] + 3*x[2]*x[6] + 
-        2*x[5]*x[6] + 4*x[1]*x[7] + 3*x[1]*x[8] + 2*x[2]*x[8],
-    x[2]*x[3] + 4*x[5]*x[7],
-    4*x[2]*x[4] + x[5]*x[8],
-    x[1]*x[5] + 4*x[1]*x[6] + 4*x[6]^2 + x[1]*x[8],
-    x[1]*x[2] + x[2]*x[4] + 4*x[1]*x[7] + 4*x[6]*x[7],
-    4*x[1]*x[7] + x[6]*x[8],
-    2*x[2]*x[5] + 3*x[2]*x[6] + 3*x[7]^2 + 2*x[2]*x[8],
-    2*x[2]*x[6] + 3*x[7]*x[8],
-    2*x[1]*x[2] + 3*x[8]^2,
-    4*x[1]*x[2] + x[2]^2 + 3*x[1]*x[3] + 4*x[2]*x[3] + 4*x[3]^2 + 3*x[1]*x[5] + 
-        3*x[2]*x[5] + 4*x[2]*x[6] + 4*x[1]*x[7] + 2*x[2]*x[7] + x[1]*x[8] + 
-        2*x[2]*x[8],
-    Curve over GF(5) defined by
-    4*x[1]*x[3] + 3*x[3]*x[5] + 4*x[1]*x[6] + 2*x[2]*x[6] + 2*x[6]*x[7] + 
-        x[1]*x[8],
-    2*x[1]*x[2] + 2*x[1]*x[4] + 2*x[2]*x[5] + 4*x[3]*x[6] + 2*x[1]*x[7],
-    2*x[2]*x[6] + 2*x[3]*x[7] + 2*x[6]*x[7] + 3*x[1]*x[8],
-    3*x[1]*x[2] + 2*x[2]*x[5] + 2*x[3]*x[8],
-    2*x[1]*x[2] + x[2]^2 + x[2]*x[4] + 3*x[4]^2 + x[2]*x[5] + x[2]*x[7],
-    x[1]*x[2] + 4*x[1]*x[4] + 2*x[2]*x[5] + 3*x[4]*x[5] + 4*x[1]*x[7],
-    2*x[2]*x[6] + 2*x[4]*x[6] + 2*x[6]*x[7] + 3*x[1]*x[8],
-    3*x[1]*x[2] + 2*x[2]*x[5] + 2*x[4]*x[7],
-    2*x[2]*x[3] + 3*x[4]*x[8],
-    3*x[1]^2 + 2*x[1]*x[2] + 2*x[1]*x[4] + x[1]*x[5] + 2*x[2]*x[5] + 4*x[5]^2,
-    2*x[1]*x[3] + x[1]*x[6] + 2*x[2]*x[6] + x[5]*x[6] + 2*x[6]*x[7] + 
-        2*x[1]*x[8],
-    2*x[1]*x[2] + 2*x[1]*x[4] + 2*x[2]*x[5] + x[1]*x[7] + x[5]*x[7],
-    2*x[2]*x[6] + 2*x[6]*x[7] + 3*x[5]*x[8],
-    3*x[1]*x[2] + 3*x[1]*x[4] + 2*x[2]*x[5] + 4*x[6]^2 + 4*x[1]*x[7] + 
-        x[2]*x[7],
-    2*x[2]*x[3] + x[3]*x[4] + 4*x[2]*x[6] + 2*x[6]*x[7] + 4*x[1]*x[8] + 
-        2*x[2]*x[8],
-    4*x[2]*x[5] + x[2]*x[7] + x[6]*x[8],
-    4*x[2]*x[5] + x[2]*x[7] + x[7]^2,
-    2*x[2]*x[6] + 3*x[7]*x[8],
-    x[2]*x[7] + 4*x[8]^2,
-    x[1]*x[2] + 2*x[3]^2 + 2*x[1]*x[4] + 2*x[2]*x[5] + 4*x[1]*x[7]
-]
-*/
-#EllipticCurve(Curve(l[1])); //4
+#l;
+
+#EllipticCurve(Curve(l[1])); //8
 #EllipticCurve(Curve(l[2])); //6
 
 Qx<x> := PolynomialRing(Rationals());
