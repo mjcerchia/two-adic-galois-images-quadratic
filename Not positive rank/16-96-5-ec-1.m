428eb704027a0fc4a239d397640d4ece76ae5631
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-24 22:31:47 -0400

    Update 16-96-5-ec-1.m

--- Not positive rank/16-96-5-ec-1.m
+++ Not positive rank/16-96-5-ec-1.m
@@ -7,8 +7,7 @@ NOT bielliptic.
 ******************************************************************************/
 P<x,y,z,w,t> := ProjectiveSpace(Rationals(),4);
 
-C := Curve(P,[2*x*z + y*w - y*t, 2*x^2 - 4*y^2 + 4*z^2 - w^2 + w*t, 2*x^2 + \
-4*y^2 + 4*z^2 - 2*w^2 + 3*w*t + t^2]);
+C := Curve(P,[2*x*z + y*w - y*t, 2*x^2 - 4*y^2 + 4*z^2 - w^2 + w*t, 2*x^2 + 4*y^2 + 4*z^2 - 2*w^2 + 3*w*t + t^2]);
 
  
 C3 := Curve(Reduction(C,3)); 
@@ -20,7 +19,7 @@ Stemp := Automorphisms(C3);
 for s in Stemp do
 auts := Append(auts, S!s);
 end for;
-#auts eq #S;
+assert #auts eq #S;
 
 
 //There are no genus one quotients by an involution
@@ -29,7 +28,7 @@ m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't 
 for g in auts do
 if Order(g) eq 2 then
 AG := AutomorphismGroup(C3,[g]);
-CG,prj := CurveQuotient(AG);prj;
+CG,prj := CurveQuotient(AG);
 if Genus(CG) eq 1 then
 try
 l := Append(l,CG);
@@ -37,9 +36,9 @@ catch e
 m := Append(m,CG);
 end try;
 end if;
-CG; Genus(CG);
+
 end if;
-print ".........";
+
 end for;
 
-l;
+l; // empty
