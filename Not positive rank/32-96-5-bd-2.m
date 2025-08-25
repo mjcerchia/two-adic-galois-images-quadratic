c629d28bb26b3d5b8927102f95cdaaeb685a0a92
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-25 08:12:22 -0400

    Update 32-96-5-bd-2.m

--- Not positive rank/32-96-5-bd-2.m
+++ Not positive rank/32-96-5-bd-2.m
@@ -16,7 +16,7 @@ Stemp := Automorphisms(C7);
 for s in Stemp do
 auts := Append(auts, S!s);
 end for;
-#auts eq #S;
+assert #auts eq #S;
 
 
 //There is one genus one quotients by an involution
@@ -25,7 +25,7 @@ m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't 
 for g in auts do
 if Order(g) eq 2 then
 AG := AutomorphismGroup(C7,[g]);
-CG,prj := CurveQuotient(AG);prj;
+CG,prj := CurveQuotient(AG);
 if Genus(CG) eq 1 then
 try
 l := Append(l,CG);
@@ -33,37 +33,13 @@ catch e
 m := Append(m,CG);
 end try;
 end if;
-CG; Genus(CG);
+
 end if;
-print ".........";
+
 end for;
 
-l;
-/*
-[
-    Curve over GF(7) defined by
-    5*x[3]*x[5] + 2*x[2]*x[7],
-    x[3]*x[6] + 6*x[7]*x[8],
-    3*x[1]*x[5] + 6*x[1]*x[7] + 2*x[2]*x[7] + x[3]*x[7],
-    2*x[2]*x[4] + 4*x[1]*x[8] + 6*x[2]*x[8] + 3*x[3]*x[8],
-    2*x[1]^2 + 3*x[1]*x[2] + 5*x[1]*x[3] + x[4]^2,
-    4*x[4]*x[5] + x[1]*x[6] + 5*x[2]*x[6] + 6*x[7]*x[8],
-    4*x[1]*x[5] + 3*x[4]*x[6],
-    4*x[1]*x[6] + 3*x[4]*x[7],
-    4*x[1]*x[2] + 3*x[4]*x[8],
-    x[1]*x[2] + 5*x[2]^2 + 6*x[2]*x[3] + 4*x[5]^2,
-    4*x[2]*x[4] + 3*x[5]*x[6],
-    4*x[1]*x[2] + 3*x[5]*x[7],
-    2*x[2]*x[6] + 5*x[5]*x[8],
-    4*x[1]*x[2] + 3*x[6]^2,
-    2*x[6]*x[7] + 5*x[1]*x[8],
-    2*x[2]*x[7] + 5*x[6]*x[8],
-    5*x[1]*x[3] + 2*x[7]^2,
-    2*x[3]*x[4] + 5*x[1]*x[8],
-    6*x[2]*x[3] + x[8]^2,
-    2*x[1]*x[2] + 4*x[1]*x[3] + 6*x[2]*x[3] + 3*x[3]^2
-]
-*/
+#l; //1
+
 #EllipticCurve(Curve(l[1])); //8
 
 Qx<x> := PolynomialRing(Rationals());
