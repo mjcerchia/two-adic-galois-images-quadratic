20267ae5975cce7c6218dce435ac3abedb95eab5
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-25 09:50:30 -0400

    Update 32-96-5-q-1.m

--- Not positive rank/32-96-5-q-1.m
+++ Not positive rank/32-96-5-q-1.m
@@ -15,7 +15,7 @@ Stemp := Automorphisms(C);
 for s in Stemp do
 auts := Append(auts, S!s);
 end for;
-#auts eq #S;
+assert #auts eq #S;
 
 
 //There is one genus one quotient by an involution
@@ -24,7 +24,7 @@ m:= []; //in case Magma complains that genus 1 curves and elliptic curves can't 
 for g in auts do
 if Order(g) eq 2 then
 AG := AutomorphismGroup(C,[g]);
-CG,prj := CurveQuotient(AG);prj;
+CG,prj := CurveQuotient(AG);
 if Genus(CG) eq 1 then
 try
 l := Append(l,CG);
@@ -32,38 +32,13 @@ catch e
 m := Append(m,CG);
 end try;
 end if;
-CG; Genus(CG);
+
 end if;
-print ".........";
+
 end for;
 
-l;
-/*
-[
-    Curve over Rational Field defined by
-    x[1]^2 + 2*x[1]*x[4] - x[4]^2 + x[6]^2 - 2*x[6]*x[7] - x[7]^2,
-    x[1]*x[2] - x[6]^2,
-    x[1]*x[3] - x[7]^2,
-    x[1]*x[5] - x[4]*x[6],
-    -x[4]*x[5] + x[1]*x[6] + 2*x[4]*x[6] + x[5]*x[8] - x[7]*x[8],
-    -x[4]*x[5] + 2*x[4]*x[6] + 2*x[1]*x[7] + x[4]*x[7] + x[5]*x[8] - x[7]*x[8],
-    -x[6]*x[7] + x[1]*x[8],
-    x[2]^2 - x[5]^2 + 9*x[6]^2 - 14*x[6]*x[7] - 2*x[3]*x[8] - 5*x[8]^2,
-    x[2]*x[3] - x[8]^2,
-    x[2]*x[4] - x[5]*x[6],
-    x[2]*x[6] - x[5]*x[8] - 2*x[6]*x[8],
-    x[2]*x[7] - x[6]*x[8],
-    -x[5]*x[6] + 4*x[6]^2 - 7*x[6]*x[7] + x[2]*x[8] - x[3]*x[8] - 2*x[8]^2,
-    x[3]^2 + x[6]^2 - 6*x[6]*x[7] + 7*x[7]^2 + 2*x[3]*x[8] - x[8]^2,
-    x[3]*x[4] - x[6]*x[7] + 2*x[7]^2,
-    x[3]*x[5] - x[6]*x[8] + 2*x[7]*x[8],
-    x[3]*x[6] - x[7]*x[8],
-    -x[4]*x[5] + 4*x[4]*x[6] + 2*x[3]*x[7] - 7*x[4]*x[7] + x[5]*x[8] - 
-        2*x[6]*x[8] + 3*x[7]*x[8],
-    -x[6]^2 + 2*x[6]*x[7] + x[4]*x[8],
-    -x[6]^2 + x[5]*x[7] + 2*x[6]*x[7]
-]
-*/
+#l; //1
+
 
 #EllipticCurve(Curve(Reduction((l[1]),7))); //8
 
