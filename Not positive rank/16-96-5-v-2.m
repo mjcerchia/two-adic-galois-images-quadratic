50bc1261706ea925dbb8c1e98ab1fc33db68396d
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-25 07:46:44 -0400

    Update 16-96-5-v-2.m

--- Not positive rank/16-96-5-v-2.m
+++ Not positive rank/16-96-5-v-2.m
@@ -1,9 +1,16 @@
-//16.96.5.v.2
+/****************************************************************************** 
+Here is a summary of the argument.
+
+Let C be the modular curve with lmfdb label 16.96.5.v.2.
+We compute the automorphism group over Q and find there is one genus one quotient by an involution. It has 4 points over F3, while the
+single rank one factor of the Jacobian has 6 points.
+NOT bielliptic. 
+******************************************************************************/
 
 P<x,y,z,w,t> := ProjectiveSpace(Rationals(),4);
 C := Curve(P,[2*y^2 - w^2 + w*t, 2*z^2 - w*t - t^2, 2*x^2 + y*z]);
 
-//Cp:=Curve(Reduction(C,5));
+
 
 G:=AutomorphismGroup(C);
 S:=Automorphisms(C);
@@ -11,8 +18,7 @@ S:=Automorphisms(C);
 for s in S do
 
 s1:=G!s;
- 
-Order(s1);
+
 if Order(s1) eq 2 then
 
 AG := AutomorphismGroup(C,[s1]);
@@ -20,7 +26,7 @@ CG,prj := CurveQuotient(AG);
 
 if Genus(CG) eq 1 then
 
-CG;
+
 Cp:=Curve(Reduction(CG,3));
 E:=EllipticCurve(Cp);
 #Points(E);
