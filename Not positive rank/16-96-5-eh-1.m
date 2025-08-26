c6eb62afb7cca0b24e251eab1341ca24245c8577
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-26 11:30:20 -0400

    Update 16-96-5-eh-1.m

--- Not positive rank/16-96-5-eh-1.m
+++ Not positive rank/16-96-5-eh-1.m
@@ -2,7 +2,7 @@
 Here is a summary of the argument.
 
 Let C be the modular curve with lmfdb label 16.96.5.eh.1.
-We compute the automorphism group using singular model and find there are two genus one quotients by involutions. One of them is pointless and its Jacobian is isomorphic to 256.2.a.a.
+We compute the automorphism group using singular model and find there are two genus one quotients by involutions. Both of them are pointless and their Jacobian is isomorphic to 256.2.a.a.
 NOT bielliptic. 
 ******************************************************************************/
 P<x,y,z>:=ProjectiveSpace(Rationals(),2);
@@ -88,6 +88,66 @@ C2:=Image(phi);
 yv^2 + 5/512*x1v^4 + 11/32*x1v^3*x2v + 37/8*x1v^2*x2v^2 + 29*x1v*x2v^3 + 74*x2v^4*/
 P<x> := PolynomialRing(Rationals());
 f := -(5/512*x^4 + 11/32*x^3 + 37/8*x^2 + 29*x + 74);
+
+
+
+H := HyperellipticCurve(f);
+IsLocallySolvable(H,2); // no Q_2 points
+Jacobian(GenusOneModel(H)); //isomorphic to 256.2.a.a
+
+
+C1:=l[2];
+
+//constructing a simple model for C1
+
+P<[X]> := AmbientSpace(C1);
+deg := 2;
+tups := {};
+d := Dimension(P);
+ideals := {@@};
+tupsOld := tups;
+coeffs := [ 0, 1 ];
+    tups   :=
+        [ P![ tup[i] : i in [1..#tup] ] :
+          tup in CartesianPower(coeffs, d+1) |
+          not tup eq < 0 : i in [1..d+1] >  
+          and not P![tup[i] : i in [1..#tup]] in tupsOld
+        ];
+    for c in tups  do  
+            L := Scheme(P,&+[c[i]*X[i] : i in [1..Dimension(AmbientSpace(C1)) + 1]]);
+              irr := IrreducibleComponents(L meet C1);
+              degs := [Degree(ReducedSubscheme(i)) : i in irr];
+              if  deg in degs then
+        for cpt in irr do  
+         if  Degree(ReducedSubscheme(cpt)) eq deg
+             and not Basis(Ideal(ReducedSubscheme(cpt))) in ideals then      
+               ideals := ideals join {@Basis(Ideal(ReducedSubscheme(cpt)))@}; 
+        end if;
+    end for;
+    end if;
+    end for;
+    gens := ideals[1];
+P7 := AmbientSpace(C1);
+R  := CoordinateRing(P7);
+I := ideal< R | gens >;
+Pl := Place(C1, I);
+D  := Divisor(Pl);
+a,b := RiemannRochSpace(D);
+x1 := b(a.1); //(the nonconstant element, equal to 1/$.1)
+x2 := b(a.2); //(the nonconstant element, equal to 1/$.1)
+a,b := RiemannRochSpace(2*D);
+y := b(a.1); //(equal to (512*$.1^4 + 1)/(4*$.1^4)*$.6, the other basis elements are 1, x, x^2)
+F := BaseField(C1);
+P := PolynomialRing( F, [2,1,1], "grevlexw", [2,1,1] );
+vars := ["yv","x1v", "x2v"]; //(calling them yv and xv so magma doesn't confuse the functions with x and y)
+P := ProjectiveSpace(P);
+AssignNames(~P, vars);
+phi := map<C1 -> P | [y,x1,x2]>;
+C2:=Image(phi);
+/*Curve over Rational Field defined by
+yv^2 + 5/512*x1v^4 - 11/32*x1v^3*x2v + 37/8*x1v^2*x2v^2 - 29*x1v*x2v^3 + 74*x2v^4*/
+P<x> := PolynomialRing(Rationals());
+f := -(5/512*x^4 - 11/32*x^3 + 37/8*x^2 - 29*x + 74);
 
 H := HyperellipticCurve(f);
 IsLocallySolvable(H,2); // no Q_2 points
