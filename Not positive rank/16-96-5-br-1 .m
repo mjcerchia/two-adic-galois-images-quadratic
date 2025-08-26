a5e8708ab457aee6717ff75a1ddf4bb833426f6c
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2025-08-26 10:43:18 -0400

    Update 16-96-5-br-1 .m

--- Not positive rank/16-96-5-br-1 .m
+++ Not positive rank/16-96-5-br-1 .m
@@ -2,7 +2,7 @@
 Here is a summary of the argument.
 
 Let C be the modular curve with lmfdb label 16.96.5.br.1.
-Using a singular model we find a pointless genus 1 quotient whose Jacobian is 256.2.a.b. This is not positive rank bielliptic. 
+Using a singular model we find two pointless genus 1 quotients whose Jacobian is 256.2.a.b. This is not positive rank bielliptic. 
 ******************************************************************************/
 
 P<x,y,z>:=ProjectiveSpace(Rationals(),2);
@@ -87,6 +87,59 @@ AssignNames(~P, vars);
 phi := map<C1 -> P | [y,x1,x2]>;
 C2:=Image(phi);
 C2;
+
+// building a simpler model for l[2]
+
+C1:=l[2];
+
+P<[X]> := AmbientSpace(C1);
+deg := 2;
+tups := {};
+d := Dimension(P);
+ideals := {@@};
+tupsOld := tups;
+coeffs := [ 0, 1 ];
+    tups   :=
+        [ P![ tup[i] : i in [1..#tup] ] :
+          tup in CartesianPower(coeffs, d+1) |
+          not tup eq < 0 : i in [1..d+1] >  
+          and not P![tup[i] : i in [1..#tup]] in tupsOld
+        ];
+    for c in tups  do  
+            L := Scheme(P,&+[c[i]*X[i] : i in [1..Dimension(AmbientSpace(C1)) + 1]]);
+              irr := IrreducibleComponents(L meet C1);
+              degs := [Degree(ReducedSubscheme(i)) : i in irr];
+              if  deg in degs then
+        for cpt in irr do  
+         if  Degree(ReducedSubscheme(cpt)) eq deg
+             and not Basis(Ideal(ReducedSubscheme(cpt))) in ideals then      
+               ideals := ideals join {@Basis(Ideal(ReducedSubscheme(cpt)))@}; 
+        end if;
+    end for;
+    end if;
+    end for;
+    
+//This was successful, so we can build a new model
+gens := ideals[3];
+P7 := AmbientSpace(C1);
+R  := CoordinateRing(P7);
+I := ideal< R | gens >;
+Pl := Place(C1, I);
+D  := Divisor(Pl);
+a,b := RiemannRochSpace(D);
+x1 := b(a.1); //(the nonconstant element, equal to 1/$.1)
+x2 := b(a.2); //(the nonconstant element, equal to 1/$.1)
+a,b := RiemannRochSpace(2*D);
+y := b(a.1); //(equal to (512*$.1^4 + 1)/(4*$.1^4)*$.6, the other basis elements are 1, x, x^2)
+F := BaseField(C1);
+P := PolynomialRing( F, [2,1,1], "grevlexw", [2,1,1] );
+vars := ["yv","x1v", "x2v"]; //(calling them yv and xv so magma doesn't confuse the functions with x and y)
+P := ProjectiveSpace(P);
+AssignNames(~P, vars);
+phi := map<C1 -> P | [y,x1,x2]>;
+C2:=Image(phi);
+C2;
+
 
 /* Curve over Rational Field defined by
 yv^2 + 2209/1595103161087639665800000000*x1v^4 - 1216/3757*yv*x1v*x2v + 369664/14115049*x1v^2*x2v^2 + 7933071522063974400000000/169*x2v^4*/
